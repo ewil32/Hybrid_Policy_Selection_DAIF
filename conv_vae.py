d4f189cb40c1bd1dbab9317d9a06d7c895b230a8
Ethan <ewil32@student.monash.edu>
2022-08-13 11:05:05 +1000

    Started working on ConvVAE
    Can't get car racing env to work but have a ConVAE implementation working

--- conv_vae.py
+++ conv_vae.py
@@ -17,7 +17,7 @@ class Sampling(layers.Layer):
         return z_mean + z_stddev * epsilon
 
 
-def create_conv_encoder(input_dim, latent_dim, conv_shapes=[(3,3), (3,3), (3,3)], num_filters=[32, 64, 64]):
+def create_conv_encoder(input_dim, latent_dim, conv_shapes=[3, 3,  3], num_filters=[32, 64, 64], dense_units=[16]):
 
     encoder_inputs = keras.Input(shape=input_dim)
 
@@ -25,7 +25,12 @@ def create_conv_encoder(input_dim, latent_dim, conv_shapes=[(3,3), (3,3), (3,3)]
     for n in range(len(conv_shapes)):
         filter_shape = conv_shapes[n]
         num = num_filters[n]
-        x = layers.Conv2D(filter_shape, )
+        x = layers.Conv2D(num, filter_shape, activation="relu", strides=2, padding="same")(x)
+
+    x = layers.Flatten()(x)
+
+    for d in dense_units:
+        x = layers.Dense(d, activation="relu")(x)
 
     z_mean = layers.Dense(latent_dim, name="z_mean")(x)
     z_log_std = layers.Dense(latent_dim, name="z_stddev")(x)  # output log of sd
@@ -36,23 +41,36 @@ def create_conv_encoder(input_dim, latent_dim, conv_shapes=[(3,3), (3,3), (3,3)]
     return encoder
 
 
-def create_conv_decoder(latent_dim, output_dim, hidden_units=[16, 8]):
+def create_conv_decoder(latent_dim, output_shape, conv_shapes=[3, 3, 3], num_filters=[64, 64, 32], dense_units=[16], rgb=True):
 
     latent_inputs = keras.Input(shape=(latent_dim,))
 
     x = latent_inputs
-    for n in hidden_units:
-        x = layers.Dense(n, activation="silu")(x)
 
-    decoder_outputs = layers.Dense(output_dim)(x)
+    for d in dense_units:
+        x = layers.Dense(d, activation="relu")(x)
+
+    first_shape = (conv_shapes[0], conv_shapes[0], num_filters[0])
+    x = layers.Reshape(first_shape)(x)
+
+    for n in range(len(conv_shapes)):
+        filter_shape = conv_shapes[n]
+        num = num_filters[n]
+        x = layers.Conv2DTranspose(num, filter_shape, activation="relu", strides=2, padding="same")(x)
+
+    if rgb:
+        decoder_outputs = x
+    else:
+        decoder_outputs = layers.Conv2DTranspose(1, 3, activation="sigmoid", padding="same")(x)
+
     decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
 
     return decoder
 
 
-class VAE(keras.Model):
+class ConvVAE(keras.Model):
     def __init__(self, encoder, decoder, latent_dim, reg_mean, reg_stddev, recon_stddev=0.05, llik_scaling=1, kl_scaling=1, **kwargs):
-        super(VAE, self).__init__(**kwargs)
+        super(ConvVAE, self).__init__(**kwargs)
         self.encoder = encoder
         self.decoder = decoder
         self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
@@ -95,15 +113,8 @@ class VAE(keras.Model):
             z_mean, z_stddev, z = self.encoder(x)
             reconstruction = self.decoder(z)
 
-            # TODO why is it not this? Why should it be log prob instead?
-            # reconstruction_loss = keras.losses.binary_crossentropy(x, reconstruction) * self.llik_scaling  # need scaling to stop collapse
-
-            # TODO fix this because it seems totally wrong
-            # prob dist of reconstruction and log prob of obs under this distribution
-            # reconstruction_dist = tfp.distributions.MultivariateNormalDiag(loc=reconstruction, scale_diag=tf.ones_like(reconstruction) * self.reconstruction_stddev)
-            # reconstruction_loss = -1 * reconstruction_dist.log_prob(x)
-
-            reconstruction_loss = nll_gaussian(reconstruction, x, self.reconstruction_stddev**2, use_consts=False) * self.llik_scaling
+            # TODO Fix this to be a real loss function
+            reconstruction_loss = nll_gaussian(reconstruction, x, use_consts=False)
 
             posterior_dist = tfp.distributions.MultivariateNormalDiag(loc=z_mean, scale_diag=z_stddev)
             reg_dist = tfp.distributions.MultivariateNormalDiag(loc=self.reg_mean, scale_diag=self.reg_stddev)
@@ -131,4 +142,4 @@ def nll_gaussian(pred, target, variance, use_consts=True):
         const = 0.5*np.log(2*np.pi*variance)
         neg_log_prob += const
 
-    return tf.reduce_sum(neg_log_prob, axis=1)
+    return tf.reduce_sum(neg_log_prob)
